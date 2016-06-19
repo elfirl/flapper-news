cb883b167f46e8e5d618908279017cf2a06c1507
Mike <duredhil@gmail.com>
2016-06-19 13:33:22 -0400

    Adding new states for posts/comments.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.factory('posts', [function() {
 	var o = {
@@ -7,17 +7,39 @@ app.factory('posts', [function() {
 	return o;
 }])
 
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			});
+
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+
+
+	}]);
+
 app.controller('MainCtrl', [
 	'$scope',
 	'posts',
 	function($scope, posts){
-		// $scope.posts = [
-		// 	{title: 'post 1', upvotes: 5},
-		// 	{title: 'post 2', upvotes: 2},
-		// 	{title: 'post 3', upvotes: 15},
-		// 	{title: 'post 4', upvotes: 9},
-		// 	{title: 'post 5', upvotes: 4}
-		// ];
 
 		$scope.posts = posts.posts;
 
@@ -28,7 +50,11 @@ app.controller('MainCtrl', [
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
